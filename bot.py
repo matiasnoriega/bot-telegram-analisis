8afa3ff26c8df3717bcbeaccac20765a930554d4
Matias <matiasnoriega86@gmail.com>
2020-07-15 21:18:27 -0300

    Importa modulos de Telegram y loggeo

--- bot.py
+++ bot.py
@@ -3,4 +3,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 SECRET_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
