eae1745c3f7d1dc3a01236ca245da20a49f26ea5
Matias <matiasnoriega86@gmail.com>
2020-07-15 21:22:23 -0300

    handlers básicos

--- bot.py
+++ bot.py
@@ -13,3 +13,14 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 SECRET_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
+
+# Definimos algunos comandos básicos que funcionaran como handlers.
+
+def start(update, context):
+    """Manda un mensaje cuando el usuario ingresa /start """
+    update.message.reply_text('Hola, bienvenido al BOT de Análisis Matemático!')
+
+
+def ayuda(update, context):
+    """Manda un mensaje cuando el usuario ingresa /ayuda """
+    update.message.reply_text('Probá el comando /factoreo para recibir los casos más comunes de factoreo!')
