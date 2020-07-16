41193b08b5af0e4205304dbb2005045d1fc0c905
Matias <matiasnoriega86@gmail.com>
2020-07-16 19:16:02 -0300

    implementa metodo derivar que toma una funcion como argumento y la deriva

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+import re
+import requests
 
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -13,6 +15,8 @@ logger = logging.getLogger(__name__)
 PORT = int(os.environ.get('PORT', 5000))
 SECRET_KEY = os.environ['TELEGRAM_BOT_API_KEY']
 HEROKU_APP = os.environ['HEROKU_APP']
+URL_API_LATEX2PNG = 'http://latex2png.com/api/convert'
+URL_BASE_LATEX2PNG = 'http://latex2png.com'
 
 # Definimos algunos comandos básicos que funcionaran como handlers.
 
@@ -26,8 +30,50 @@ def factoreo(update, context):
 
 def ayuda(update, context):
     """Manda un mensaje cuando el usuario ingresa /ayuda """
-    update.message.reply_text('Probá el comando /factoreo para recibir los casos más comunes de factoreo!')
+    update.message.reply_text('Probá el comando /factoreo para recibir los casos más comunes de factoreo o /derivar funcion para ver la derivada de f(x)!')
 
+def derivar(update, context):
+    """ Toma la función base y muestra su derivada """
+    #se declara un diccionario de patrones para contrastar la funcion recibida
+    patron_funciones = {
+        '^[0-9]+$': '0',
+        'x$': '1',
+        '': '',
+    }
+
+    #Tomamos el primer argumento que se le pasa al comando como función f(x)
+    funcion = context.args[0]
+    resultado = None
+
+    for key in patron_funciones.keys():
+        #compilamos el patron de esta entrada particular del diccionario para compararlo con la funcion recibida
+        #y guardamos el resultado en la variable match
+        regex_matcher = re.compile(key)
+        match = regex_matcher.match(funcion)
+        #si hubo una coincidencia, guardamos en la variable resultado el valor asociado a esa clave en el diccionario
+        #devolvemos resultado y detenemos el ciclo for
+        if match:
+            resultado = patron_funciones.get(key)
+            break
+
+    # Si encuentra una coincidencia usa la API de Latex2PNG
+    if(resultado):
+
+        # Data para mandar a la API de latex2png
+        payload = "{\n\"auth\": {\n\"user\": \"guest\",\n\"password\": \"guest\"\n},\n\"latex\": \"%(latex)s\" ,\n\"resolution\": 600,\n\"color\": \"111111\"\n}"%{'latex': resultado}
+        # Headers del Request
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        r = requests.request("POST", 'http://latex2png.com/api/convert', headers=headers, data=payload)
+        
+        json_response = r.json()
+        img_url = json_response["url"]
+        update.message.reply_photo('http://latex2png.com' + img_url)
+
+    else:
+        update.message.reply_text('No encontré una función base que coincida con '+ context.args[0])
 
 def error(update, context):
     """Loggea los errores causados por el Updater."""
@@ -46,6 +92,7 @@ def main():
     disp.add_handler(CommandHandler("saludo", saludo))
     disp.add_handler(CommandHandler("ayuda", ayuda))
     disp.add_handler(CommandHandler("factoreo", factoreo))
+    disp.add_handler(CommandHandler("derivar", derivar, pass_args=True))
     
     # loggea todos los errores
     disp.add_error_handler(error)
